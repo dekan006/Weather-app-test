25463b410aafe077f5bdc4f0ae0779fffc969c09
dekan06 <dekan06@gmail.com>
2022-04-05 23:35:36 +0300

    add component/card, get cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'weather-app-test';
+  showModal: boolean = false;
+  cities: Array<string> = [];
+
+  constructor(
+    private http: HttpClient
+    ) { }
+
+  ngOnInit(): void {
+    this.http.get<Object>('assets/cities.json').subscribe((res: any) =>
+      { this.cities = res.cities }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
